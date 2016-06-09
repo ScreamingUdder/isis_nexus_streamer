5611515a40dfb237e775a4d645505ef3a0955cb7
Matthew D Jones <matthew.d.jones@tessella.com>
2016-06-09 11:52:59 +0100

    try adding travis build configuration

---
+++ travis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "....fetching librdkafka dependency...."
+mkdir tmp_build
+cd tmp_build
+git clone https://github.com/edenhill/librdkafka.git
+echo ".....done....."
+cd librdkafka
+echo "....compiling librdkafka...."
+./configure && make
+sudo make install
+echo "....done...."
+cd ../../
+echo ".... ensure librdkafka is available....."
+sudo ldconfig
+rm -Rf tmp_build
