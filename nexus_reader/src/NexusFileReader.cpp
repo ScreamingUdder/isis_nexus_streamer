11af1b5c358a2a6105a2e52af9aea8af7770138e
Matthew D Jones <matthew.d.jones@tessella.com>
2016-06-10 12:19:16 +0100

    use initialisation list rather than constructor body

--- nexus_reader/src/NexusFileReader.cpp
+++ nexus_reader/src/NexusFileReader.cpp
@@ -3,8 +3,8 @@
 
 using namespace H5;
 
-NexusFileReader::NexusFileReader(const std::string &filename) {
-  m_file = std::make_shared<H5File>(filename, H5F_ACC_RDONLY);
+NexusFileReader::NexusFileReader(const std::string &filename)
+    : m_file(std::make_shared<H5File>(filename, H5F_ACC_RDONLY)) {
   DataSet dataset = m_file->openDataSet("/raw_data_1/good_frames");
   size_t *numOfFrames = new size_t[1];
   dataset.read(numOfFrames, PredType::NATIVE_UINT64);
