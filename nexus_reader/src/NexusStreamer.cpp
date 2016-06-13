9b99e9a6609d62d9817f385de81d6823321f8ccd
Matthew D Jones <matthew.d.jones@tessella.com>
2016-06-13 10:59:44 +0100

    add command line options to set filename, broker and topic

--- nexus_reader/src/NexusStreamer.cpp
+++ nexus_reader/src/NexusStreamer.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "NexusStreamer.h"
 
 NexusStreamer::NexusStreamer(std::shared_ptr<EventPublisher> publisher,
--- nexus_reader/src/main.cpp
+++ nexus_reader/src/main.cpp
@@ -1,10 +1,54 @@
+#ifdef _MSC_VER
+#include "../win32/wingetopt.h"
+#elif _AIX
+#include <unistd.h>
+#else
+#include <getopt.h>
+#endif
+#include <iostream>
+
 #include "KafkaEventPublisher.h"
 #include "NexusStreamer.h"
 
-int main() {
+int main(int argc, char **argv) {
+
+  int opt;
+  std::string filename;
+  std::string broker = "sakura";
+  std::string topic = "test_topic";
+
+  while ((opt = getopt(argc, argv, "f:b:t")) != -1) {
+    switch (opt) {
+
+    case 'f':
+      filename = optarg;
+      break;
+
+    case 'b':
+      broker = optarg;
+      break;
+
+    case 't':
+      topic = optarg;
+      break;
+
+    default:
+      goto usage;
+    }
+  }
+
+  if (filename.empty()) {
+    usage:
+    fprintf(stderr,
+            "Usage: %s -f <filepath> "
+                "[-b <host:port>] "
+                "[-t <topic_name>]"
+                "\n", argv[0]);
+    exit(1);
+  }
 
   auto publisher = std::make_shared<KafkaEventPublisher>();
-  NexusStreamer streamer(publisher, "sakura", "test_topic");
+  NexusStreamer streamer(publisher, broker, topic, filename);
 
   return 0;
 }
