c66faa9b41ac91c64ad900477fc85db04931d7b9
Matthew D Jones <matthew.d.jones@tessella.com>
2016-07-08 17:24:16 +0100

    write all required datasets to uncompressed hdf5 file

--- data/convert_hdf5_file_to_uncompressed.py
+++ data/convert_hdf5_file_to_uncompressed.py
@@ -3,10 +3,22 @@ import h5py
 """
 Read data from compressed nexus file and write it to a new uncompressed one.
 """
-with h5py.File('SANS_test.nxs', 'r') as f_read:
-    data_1 = f_read.get('/raw_data_1/detector_1_events/event_frame_number')
 
-    with h5py.File('SANS_test_uncompressed.hdf5', 'w') as f_write:
-        print(data_1[...])
-        g = f_write.create_group('/raw_data_1/detector_1_events')
-        g['event_frame_number'] = data_1[...]
+datasets = [
+    '/raw_data_1/detector_1_events/event_id',
+    '/raw_data_1/detector_1_events/total_counts',
+    '/raw_data_1/detector_1_events/event_index',
+    '/raw_data_1/good_frames',
+    '/raw_data_1/detector_1_events/event_time_offset'
+]
+
+with h5py.File('SANS_test_uncompressed.hdf5', 'w') as f_write:
+    pass
+
+with h5py.File('SANS_test.nxs', 'r') as f_read:
+    for dataset in datasets:
+        data_1 = f_read.get(dataset)
+
+        with h5py.File('SANS_test_uncompressed.hdf5', 'r+') as f_write:
+            print(data_1[...])
+            f_write[dataset] = data_1[...]
