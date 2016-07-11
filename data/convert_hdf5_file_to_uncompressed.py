683c4c33b1e1b14d387f4dd9adff09aafcb62083
Matthew D Jones <matthew.d.jones@tessella.com>
2016-07-11 09:22:13 +0100

    refactor out filenames in nexus compression conversion script

--- data/convert_hdf5_file_to_uncompressed.py
+++ data/convert_hdf5_file_to_uncompressed.py
@@ -4,6 +4,9 @@ import h5py
 Read data from compressed nexus file and write it to a new uncompressed one.
 """
 
+compressed_file = 'SANS_test.nxs'
+uncompressed_file = 'SANS_test_uncompressed.hdf5'
+
 datasets = [
     '/raw_data_1/detector_1_events/event_id',
     '/raw_data_1/detector_1_events/total_counts',
@@ -12,13 +15,13 @@ datasets = [
     '/raw_data_1/detector_1_events/event_time_offset'
 ]
 
-with h5py.File('SANS_test_uncompressed.hdf5', 'w') as f_write:
+with h5py.File(uncompressed_file, 'w') as f_write:
     pass
 
-with h5py.File('SANS_test.nxs', 'r') as f_read:
+with h5py.File(compressed_file, 'r') as f_read:
     for dataset in datasets:
         data_1 = f_read.get(dataset)
 
-        with h5py.File('SANS_test_uncompressed.hdf5', 'r+') as f_write:
+        with h5py.File(uncompressed_file, 'r+') as f_write:
             print(data_1[...])
             f_write[dataset] = data_1[...]
