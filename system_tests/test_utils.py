4684b0e64cf4a8b1178d4f7d43db2c7aa00ef968
Matthew D Jones <matthew.d.jones@tessella.com>
2016-07-25 13:25:07 +0100

    avoid failure of system tests if jmxterm response fails

--- system_tests/test_utils.py
+++ system_tests/test_utils.py
@@ -106,12 +106,16 @@ class JmxMetrics:
         self.jmxterm.sendline("open " + connection)
         self.jmxterm.expect_exact("#Connection to " + connection + " is opened", connection_timeout)
 
-    def get_metric(self, bean_type, bean_name, topic, bean_value):
-        self.jmxterm.sendline(
-            "get -b kafka.server:type=" + bean_type + ",name=" + bean_name + ",topic=" + topic + " " + bean_value)
+    def get_metric(self, bean_type, bean_name, bean_value):
+        request = "get -b kafka.server:type=" + bean_type + ",name=" + bean_name + " " + bean_value
+        self.jmxterm.sendline(request)
         response_lines = [self.jmxterm.readline(), self.jmxterm.readline()]
         while response_lines and response_lines[-1] != "\r\n":
-            response_lines.append(self.jmxterm.readline())
+            try:
+                response_lines.append(self.jmxterm.readline())
+            except pexpect.exceptions.TIMEOUT:
+                print("Response to metric value request timed out.")
+                return response_lines
 
         return response_lines
 
