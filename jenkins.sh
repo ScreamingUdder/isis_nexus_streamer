43db1bf2655adb11dbed4c999cdae8ccb3eebc58
Afonso Haruo Carnielli Mukai <afonsoharuo@outlook.com>
2016-12-21 13:03:18 +0100

    Add Jenkins build script

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+echo "Run cmake..."
+HDF5_ROOT=/opt/dm_group/hdf5 \
+    cmake ../code \
+    -DLibRDKafka_ROOT_DIR=/opt/dm_group/librdkafka
+
+echo "Build project"
+make
+
+echo "Run tests"
+./unitTests ../code/data/ --gtest_output=xml:test_results.xml
+
+echo "Create docs"
+make docs
