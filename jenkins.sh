b6b2e326efa1ba57e8cca6eaad951f666613de99
Afonso Haruo Carnielli Mukai <afonsoharuo@outlook.com>
2017-01-10 13:16:15 +0100

    Fix version number in package
    Now rpm version matches version in Doxygen documentation.

--- jenkins.sh
+++ jenkins.sh
@@ -26,11 +26,11 @@ echo "Create RPM"
 rm -rf package/*
 mkdir -p package/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
-cp build/isis_nexus_streamer-1.0.0.tar.gz package/SOURCES/
+cp build/isis_nexus_streamer-0.1.0.tar.gz package/SOURCES/
 cp code/isis_nexus_streamer.spec package/SPECS/
 
 rpmbuild \
     --define "_topdir $(pwd)/package" \
-    --define "_version 1.0.0" \
+    --define "_version 0.1.0" \
     --define "_release $BUILD_NUMBER" \
     -bb package/SPECS/isis_nexus_streamer.spec
